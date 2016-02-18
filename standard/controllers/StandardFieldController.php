a4a0419caed1fe948041f19d154688f18fb5a054
Marius Lundbø <marius.lundbo@gmail.com>
2016-02-18 14:54:58 +0100

    init

---
+++ standard/controllers/StandardFieldController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class StandardFieldController implements iController
+{
+
+
+    //TODO continue here
+
+    public function getResponse()
+    {
+        // TODO: Implement getResponse() method.
+    }
+}
