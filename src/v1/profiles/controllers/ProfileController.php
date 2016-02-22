4c583225d2e91cbbf523faca25f438d9033b1704
Marius Lundbø <marius.lundbo@gmail.com>
2016-02-22 14:14:55 +0100

    init ProfileController

---
+++ src/v1/profiles/controllers/ProfileController.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Marius
+ * Date: 22-Feb-16
+ * Time: 14:12
+ */
+class ProfileController implements iController
+{
+    public function getResponse()
+    {
+        // TODO: Implement getResponse() method.
+    }
+
+
+}
