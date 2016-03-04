f834793452dfad7b454e2260ac78d1c9dd4b5501
Andreas Kjerstad <andreas.pi@hotmail.com>
2016-03-04 15:13:12 +0100

    Added LinkType model

---
+++ src/v1/models/LinkType.php
@@ -0,0 +1,56 @@
+<?php
+
+class LinkType
+{
+    private $id, $name, $description;
+
+    /**
+     * Status constructor.
+     * @param $id
+     * @param $name
+     * @param $description
+     */
+    public function __construct($id, $name, $description)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * Returns associated array
+     * @return array
+     */
+    public function toArray()
+    {
+        return array(
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description);
+    }
+}
