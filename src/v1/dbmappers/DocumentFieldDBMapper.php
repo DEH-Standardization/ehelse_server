17ce8fde91ca6b595832b1d6ddf93a1291edae53
Andreas Kjerstad <andreas.pi@hotmail.com>
2016-03-07 13:46:13 +0100

    Created DocumentFieldDBMapper

---
+++ src/v1/dbmappers/DocumentFieldDBMapper.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once 'DBMapper.php';
+
+class DocumentFieldDBMapper extends DBMapper
+{
+    public function get($target_group)
+    {
+        // TODO: Implement get() method.
+    }
+
+    public function getById($id)
+    {
+        // TODO: Implement getById() method.
+    }
+
+    public function getAll()
+    {
+        // TODO: Implement getAll() method.
+    }
+
+    public function getAllIds()
+    {
+        // TODO: Implement getAllIds() method.
+    }
+
+    public function add($model)
+    {
+        // TODO: Implement add() method.
+    }
+
+    public function update($model)
+    {
+        // TODO: Implement update() method.
+    }
+
+
+}
--- src/v1/dbmappers/iDBMapper.php
+++ src/v1/dbmappers/iDBMapper.php
@@ -7,5 +7,6 @@ interface iDBMapper
 {
     public function add($model);        // Add element to DB.
     public function update($model);     // Update element in DB.
-    public function getAllIds();           // Return all (newest version) of element form DB.
+    public function getAll();           // Return all (newest version) of element form DB.
+    //public function getAllIds();        // Return all ids (newest version) of element form DB.
 }
