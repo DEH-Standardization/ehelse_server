fe4679ad90ce8b2a3da303d7847c8e6edd96424d
Andreas Kjerstad <andreas.pi@hotmail.com>
2016-03-19 14:56:38 +0100

    Fixed document_type_id

--- src/v1/models/DocumentField.php
+++ src/v1/models/DocumentField.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/ModelValidation.php';
 
 class DocumentField
 {
-    private $id, $name, $description, $sequence, $mandatory, $is_standard_field, $is_profiles_field;
+    private $id, $name, $description, $sequence, $mandatory, $document_type_id;
 
     /**
      * DocumentField constructor.
@@ -13,18 +13,17 @@ class DocumentField
      * @param $description
      * @param $sequence
      * @param $mandatory
-     * @param $is_standard_field
+     * @param $document_type_id
      * @param $is_profiles_field
      */
-    public function __construct($id, $name, $description, $sequence, $mandatory, $is_standard_field, $is_profile_field)
+    public function __construct($id, $name, $description, $sequence, $mandatory, $document_type_id)
     {
         $this->id = $id;
         $this->name = $name;
         $this->description = $description;
         $this->sequence = $sequence;
         $this->mandatory = $mandatory;
-        $this->is_standard_field = $is_standard_field;
-        $this->is_profile_field = $is_profile_field;
+        $this->document_type_id = $document_type_id;
     }
 
     public function getId()
@@ -84,24 +83,14 @@ class DocumentField
         $this->mandatory = $mandatory;
     }
 
-    public function getIsStandardField()
+    public function getDocumentTypeId()
     {
-        return $this->is_standard_field;
+        return $this->document_type_id;
     }
 
-    public function setIsStandardField($is_standard_field)
+    public function setDocumentTypeId($document_type_id)
     {
-        $this->is_standard_field = $is_standard_field;
-    }
-
-    public function getIsProfileField()
-    {
-        return $this->is_profile_field;
-    }
-
-    public function setIsProfileField($is_profile_field)
-    {
-        $this->is_profile_field = $is_profile_field;
+        $this->document_type_id = $document_type_id;
     }
 
     public function toArray()
@@ -112,8 +101,7 @@ class DocumentField
             'description' => $this->description,
             'sequence' => $this->sequence,
             'mandatory' => $this->mandatory,
-            'isStandardField' => $this->is_standard_field,
-            'isProfileField' => $this->is_profile_field);
+            'documentTypeId' => $this->document_type_id);
         return $assoc;
     }
 
