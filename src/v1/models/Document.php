c32a185d09f708d728a96711802334a2377e378f
Andreas Kjerstad <andreas.pi@hotmail.com>
2016-03-19 12:41:38 +0100

    Adde document model

---
+++ src/v1/models/Document.php
@@ -0,0 +1,148 @@
+<?php
+
+require_once __DIR__ . '\ModelValidation.php';
+
+class Document
+{
+    private $id, $timestamp, $title, $description, $sequence, $topic_id, $comment, $status_id, $document_type_id;
+
+    /**
+     * Document constructor.
+     * @param $id
+     * @param $timestamp
+     * @param $title
+     * @param $description
+     * @param $sequence
+     * @param $topic_id
+     * @param $comment
+     * @param $status_id
+     * @param $document_type_id
+     */
+    public function __construct($id, $timestamp, $title, $description, $sequence, $topic_id, $comment, $status_id, $document_type_id)
+    {
+        $this->id = $id;
+        $this->timestamp = $timestamp;
+        $this->setTitle($title);
+        $this->setDescription($description);
+        $this->sequence = $sequence;
+        $this->topic_id = $topic_id;
+        $this->setComment($comment);
+        $this->status_id = $status_id;
+        $this->document_type_id = $document_type_id;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getTimestamp()
+    {
+        return $this->timestamp;
+    }
+
+    public function setTimestamp($timestamp)
+    {
+        $this->timestamp = $timestamp;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Sets title if it is valid
+     * @param $description
+     */
+    public function  setTitle($title)
+    {
+        if (strlen($title) > ModelValidation::getTitleMaxLength()) {
+            $this->title = ModelValidation::getValidTitle($title);
+            echo "Title is too long. Title set to: " . $this->title;
+        } else {
+            $this->title = $title;
+        }
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Sets description if it is valid
+     * @param $description
+     */
+    public function setDescription($description)
+    {
+        if (strlen($description) > ModelValidation::getDescriptionMaxLength($description)) {
+            $this->description = ModelValidation::getValidDescription($description);
+            echo "description is too long. Description set to: " . $this->description;
+        }
+        else {
+            $this->description = $description;
+        }
+    }
+
+    public function getSequence()
+    {
+        return $this->sequence;
+    }
+
+    public function setSequence($sequence)
+    {
+        $this->sequence = $sequence;
+    }
+
+    public function getTopicId()
+    {
+        return $this->topic_id;
+    }
+
+    public function setTopicId($topic_id)
+    {
+        $this->topic_id = $topic_id;
+    }
+
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    /**
+     * Sets comment if it is valid
+     * @param $description
+     */
+    public function setComment($comment)
+    {
+        if (strlen($comment) > ModelValidation::getCommentMaxLength()) {
+            $this->comment = ModelValidation::getValidComment($comment);
+            echo "Comment is too long. Comment set to: " . $this->comment;
+        }
+        else {
+            $this->comment = $comment;
+        }
+    }
+
+    public function getStatusId()
+    {
+        return $this->status_id;
+    }
+
+    public function setStatusId($status_id)
+    {
+        $this->status_id = $status_id;
+    }
+
+    public function getDocumentTypeId()
+    {
+        return $this->document_type_id;
+    }
+
+    public function setDocumentTypeId($document_type_id)
+    {
+        $this->document_type_id = $document_type_id;
+    }
+
+}
