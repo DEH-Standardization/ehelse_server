89c44f6c58ebe3b06a1c3f8322dd8000b835dba5
william <william.henriksen@ntnu.no>
2016-04-11 11:48:59 +0200

    Password Timestamps
    Marius får det Marius vil ha

--- src/v1/models/User.php
+++ src/v1/models/User.php
@@ -7,7 +7,7 @@ class User
     const REQUIRED_PUT_FIELDS = ['name', 'email'];
     const REQUIRED_PASSWORD_PUT_FIELD = ['password'];
     const SQL_INSERT_STATEMENT = "INSERT INTO user(name,profile_image,email,pw_date_edited) VALUES (:name,:profile_image,:email,NOW());";
-    const SQL_UPDATE_STATEMENT = "UPDATE user SET name=:name,profile_image=:profile_image,email=:email,pw_date_edited=now() WHERE id = :id;";
+    const SQL_UPDATE_STATEMENT = "UPDATE user SET name=:name,profile_image=:profile_image,email=:email,pw_date_edited=NOW() WHERE id = :id;";
     const SQL_UPDATE_PASSWORD_STATEMENT = "UPDATE user SET password_hash=:password_hash, pw_date_edited=NOW() WHERE id = :id;";
     const SQL_GET_USER_BY_EMAIL = "SELECT * FROM user WHERE email=:email";
     const SQL_DELETE_USER_BY_ID = "DELETE FROM user WHERE id=:id";
@@ -144,7 +144,8 @@ class User
             null,
             null,
             null,
-            $password_hash
+            $password_hash,
+            null
         );
     }
 
