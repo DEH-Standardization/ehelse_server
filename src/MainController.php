fba81234d2f526cac0a358e0f0e4b87edbbc7243
Andreas Kjerstad <andreas.pi@hotmail.com>
2016-05-09 10:54:12 +0200

    Added authentication
    Auth is required for all POST, PUT and DELETE, unless fro POST to
    '/users/reset-password'

--- src/MainController.php
+++ src/MainController.php
@@ -8,6 +8,12 @@ class MainController implements iController
 {
     private $controller, $api_version, $path;
 
+    /**
+     * MainController constructor.
+     * @param $path
+     * @param $method
+     * @param $body
+     */
     public function __construct($path, $method, $body)
     {
         $this->path = $path;
@@ -25,16 +31,35 @@ class MainController implements iController
         }
         $path = trimPath($path, 1);
 
-        switch ($this->api_version) {
-            case 'v1':
-                $this->controller = new APIV1Controller($path, $method, $body);
-                break;
-            default:
-                $this->controller = new ErrorController(new InvalidPathError());
-                break;
+        $requires_authentication = true;
+
+        // GET, OPTIONS and POST to '/users/reset-password' does not require authentication
+        if ($method == Response::REQUEST_METHOD_GET || $method == Response::REQUEST_METHOD_OPTIONS ||
+            ($method == Response::REQUEST_METHOD_POST && $path[0] == 'users' && $path[1] == 'reset-password')
+        ) {
+            $requires_authentication = false;
+        }
+
+        // if user is not set and method requires authentication, returns AuthenticationError
+        if (!array_key_exists('CURRENT_USER', $GLOBALS) && $requires_authentication) {
+            $this->controller = new ErrorController(new AuthenticationError($method));
+        } else {
+            // check api version
+            switch ($this->api_version) {
+                case 'v1':
+                    $this->controller = new APIV1Controller($path, $method, $body);
+                    break;
+                default:
+                    $this->controller = new ErrorController(new InvalidPathError());
+                    break;
+            }
         }
     }
 
+    /**
+     * Returns response from controller
+     * @return ErrorResponse
+     */
     public function getResponse()
     {
         return $this->controller->getResponse();
