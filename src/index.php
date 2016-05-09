142d4656462807da7fca12444efe542f8ccb6de1
Andreas Kjerstad <andreas.pi@hotmail.com>
2016-05-09 11:31:00 +0200

    Added comments to index

--- src/index.php
+++ src/index.php
@@ -3,11 +3,15 @@ require_once "MainController.php";
 require_once "v1/errors/InvalidJSONError.php";
 require_once 'utils.php';
 
+// Headers
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: accept, authorization, content-type");
 header("Access-Control-Allow-Methods: PUT, DELETE, POST, GET, OPTIONS");
 
+// Store request method
 $method = $_SERVER['REQUEST_METHOD'];
+
+// Store path
 if (array_key_exists("PATH_INFO", $_SERVER)) {
     $path = explode('/', $_SERVER['PATH_INFO']);
     $path = trimPath($path, 1);
@@ -15,6 +19,7 @@ if (array_key_exists("PATH_INFO", $_SERVER)) {
     $path = array();
 }
 
+// Store payload - JSON that is sent with the request
 $payload = file_get_contents('php://input');
 if ($payload) {
     $body = json_decode($payload, true);
@@ -22,7 +27,7 @@ if ($payload) {
     $body = array();
 }
 
-
+// If there are no errors in the JSON, instantiate MainController
 if (json_last_error() == JSON_ERROR_NONE) {
     $req = new MainController($path, $method, $body);
     $response = $req->getResponse();
@@ -30,6 +35,9 @@ if (json_last_error() == JSON_ERROR_NONE) {
     $response = new ErrorResponse(new InvalidJSONError($payload));
 }
 
+// Set content type and response code for the result
 header($response->getContentType());
 http_response_code($response->getResponseCode());
+
+// Print the JSON result of request
 echo $response->getBody();
