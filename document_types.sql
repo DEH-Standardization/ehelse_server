e11b41b53498e894dc6e35cc513b403fecacbcd8
Dag Frode Solberg <tncditto@gmail.com>
2016-05-26 20:57:13 +0200

    Added db files

---
+++ document_types.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM ehelse.document_type
+LIMIT 0, 1000
+
+-- Date: 2016-05-26 20:56
+*/
+INSERT INTO `document_type` (`id`,`name`) VALUES (1,'Standard');
+INSERT INTO `document_type` (`id`,`name`) VALUES (2,'Profil');
+INSERT INTO `document_type` (`id`,`name`) VALUES (3,'Støttedokument');
