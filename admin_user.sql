bc4ede3c7f54cd64786c2971b09c05cac5b61331
Dag Frode Solberg <tncditto@gmail.com>
2016-05-26 23:03:01 +0200

    Added default user sql

---
+++ admin_user.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM ehelse.user
+LIMIT 0, 1000
+
+-- Date: 2016-05-26 23:02
+*/
+USE `ehelse` ;
+INSERT INTO `user` (`id`,`name`,`profile_image`,`email`,`password_hash`,) VALUES (48,'Admin',NULL,'admin','$2y$10$KzUB/s7o1dPuFdhqJrs4muC9F0OzaiEIeb2ypQcsZUVQQ4kiSCQ42');
